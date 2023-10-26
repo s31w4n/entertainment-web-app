8209164719b34209d1a916bbddf1617143a05d52
s31w4n <sewan.khalni@gmail.com>
2023-10-26 19:52:52 +0330

    FIX: DetailsImage page errors

--- src/components/DetailsImage.tsx
+++ src/components/DetailsImage.tsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { BookmarkButton, Notification } from "@/components";
 import { DetailsImageProps as T } from "@/types";
-import { useBookmark, useNotification } from "@/hooks";
+import { useNotification, useHttp } from "@/hooks";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { authActions } from "@/features/auth/authSlice";
+import { messageActions } from "@/features/message/messageSlice";
 
 const DetailsImage: React.FC<T> = ({ id, poster_path, title }) => {
   const { notification, handleNotification } = useNotification();
-  const { isBookmarking, handleBookmark } = useBookmark({
-    id,
-    handleNotification,
-  });
+  const dispatch = useAppDispatch();
+  const { sendRequest } = useHttp();
+  const { userId, bookmarks, token } = useAppSelector((state) => state.auth);
+  const [isBookmarking, setIsBookmarking] = useState(false);
+
+  const isBookmarked = bookmarks.includes(id);
+
+  const bookMarkHandler = () => {
+    if (!userId) {
+      dispatch(messageActions.showLoginMessage());
+      return;
+    }
+
+    try {
+      setIsBookmarking(true);
+      sendRequest({
+        url: "/api/bookmark",
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          userId,
+          itemId: id,
+          operation: isBookmarked ? "pull" : "push",
+        }),
+      });
+
+      dispatch(
+        authActions.updateBookmarks({
+          id,
+          operation: isBookmarked ? "remove" : "add",
+        }),
+      );
+      setIsBookmarking(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="relative h-[525px] w-[350px]">
@@ -23,8 +62,9 @@ const DetailsImage: React.FC<T> = ({ id, poster_path, title }) => {
       />
       <BookmarkButton
         id={id}
+        isBookmarked={isBookmarked}
         isBookmarking={isBookmarking}
-        onClick={handleBookmark}
+        onClick={bookMarkHandler}
       />
       {notification.active && (
         <Notification

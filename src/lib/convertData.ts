bbc79e03ecf86886bf1000222128325ead46993a
s31w4n <sewan.khalni@gmail.com>
2023-10-27 19:06:22 +0330

    Fix: authentication

--- src/lib/convertData.ts
+++
@@ -1,7 +0,0 @@
-const convertData = (doc: any) => {
-  const convertedData = doc.toObject();
-  convertedData._id = convertedData._id.toString();
-  return convertedData;
-};
-
-export default convertData;

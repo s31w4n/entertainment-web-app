337920814629e9a8c39e9f68ded82b9cad2e0efb
s31w4n <sewan.khalni@gmail.com>
2023-10-31 19:45:52 +0330

    add profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -21,8 +21,8 @@ const Profile: NextPage<T> = ({ session }) => {
   return (
     <section className="mt-10 sm:mt-16">
       <Heading title={`You are logged in as ${userEmail}.`} />
-      <Heading title="You can change your password or logout." />
-      <div className="flex justify-center mt-[80px] sm:mt-[50px] lg:mt-[119px]">
+      <Heading title="You can change your password below." />
+      <div className="mt-[80px] flex justify-center sm:mt-[50px] lg:mt-[119px]">
         <div className="relative h-auto w-[327px] rounded-lg bg-app-semi-dark-blue p-6 sm:w-[400px] sm:p-8">
           <div className="mb-10">
             <h1 className="text-app-heading-lg font-light">Change password</h1>
